d6ab32ae429829722b1dc734f7a9d86cae2779a0
qkeave <qkeave@gmail.com>
2015-10-07 17:23:18 -0700

    basics

---
+++ footer.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * The template for displaying the footer.
+ *
+ * Contains the closing of the #content div and all content after.
+ *
+ * @link https://developer.wordpress.org/themes/basics/template-files/#template-partials
+ */
+
+?>
+
+			</div><!-- #content -->
+		</div><!-- #page -->
+		<footer id="colophon" class="site-footer container section" role="contentinfo">
+
+		</footer><!-- #colophon -->
+
+		<!-- CDN Dependencies -->
+		<script src="//ajax.googleapis.com/ajax/libs/jquery/2.0.0/jquery.min.js"></script>
+		<script src="//ajax.googleapis.com/ajax/libs/jqueryui/1.10.3/jquery-ui.min.js"></script>
+		<script src="//cdnjs.cloudflare.com/ajax/libs/underscore.js/1.6.0/underscore-min.js"></script>
+		<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.2.0/css/font-awesome.min.css" rel="stylesheet">
+		<script src="https://use.typekit.net/qlo3pfx.js"></script>
+		<script>try{Typekit.load({ async: true });}catch(e){}</script>
+
+		<!-- Local Dependencies -->
+		<!-- Typography -->
+		<script type="text/javascript" src="<?php echo $tRoot ?>/js/jQuery-widowFix/js/jquery.widowFix-1.3.2.min.js" ></script>
+		<script src="<?php echo $tRoot ?>/js/smartquotes/smartquotes.min.js"></script>
+		<script src="<?php echo $tRoot ?>/js/hypher/jquery.hypher.js"></script>
+		<script src="<?php echo $tRoot ?>/js/hypher/en-us.js"></script>
+
+		<!-- MAIN JS -->
+		<script src="<?php echo $tRoot ?>/src/js/main.js"></script>
+
+		<!-- Google Analytics: change UA-XXXXX-X to be your site's ID. -->
+		<!-- (Via HTML5 Boilerplate: http://html5boilerplate.com/) -->
+		<script>
+			var _gaq=[['_setAccount','UA-XXXXX-X'],['_trackPageview']];
+			(function(d,t){var g=d.createElement(t),s=d.getElementsByTagName(t)[0];
+			g.src=('https:'==location.protocol?'//ssl':'//www')+'.google-analytics.com/ga.js';
+			s.parentNode.insertBefore(g,s)}(document,'script'));
+		</script>
+
+		<?php wp_footer(); ?>
+
+
+	</body>
+</html>
---
+++ header.php
@@ -0,0 +1,66 @@
+<?php
+/**
+ * The header for our theme.
+ *
+ * Displays all of the <head> section and everything up till <div id="content">
+ */
+?>
+<!DOCTYPE html>
+<html <?php language_attributes(); ?>>
+	<head>
+		<meta charset="<?php bloginfo( 'charset' ); ?>">
+		<meta name="viewport" content="width=device-width, initial-scale=1.0">
+		<title> </title>
+		<link rel="profile" href="http://gmpg.org/xfn/11">
+		<link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
+
+		<!-- Force latest available IE rendering engine and Chrome Frame (if installed) -->
+		<meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1">
+
+		<!-- OGG Title & Description -->
+		<meta property="og:site_name" content=""/>
+		<meta property="og:title" content="<?php wp_title( '|', true, 'right' ); ?>"/>
+		<?php //Adding the Open Graph in the Language Attributes
+		function add_opengraph_doctype( $output ) {
+				return $output . ' xmlns:og="http://opengraphprotocol.org/schema/" xmlns:fb="http://www.facebook.com/2008/fbml"';
+			}
+		add_filter('language_attributes', 'add_opengraph_doctype');
+		?>
+		<!-- Favicon -->
+		<link rel="shortcut icon" type="image/ico" href="<?php echo get_bloginfo('template_directory'); ?>/fav.ico">
+
+		<?php wp_head(); ?>
+
+		<!-- Stylesheet -->
+		<link rel="stylesheet" href="<?php echo get_bloginfo('template_directory');?>/styles/style.css" type="text/css">
+
+		<!-- HTML5 Shim for IE 6-8 -->
+		<!--[if lt IE 9]>
+			<script src="http://html5shiv.googlecode.com/svn/trunk/html5.js"></script>
+		<![endif]-->
+
+	</head>
+
+	<body <?php body_class(); ?> ng-controller="chapter_ctrl">
+
+		<?php 
+			if( !is_page_template('page-content-dump.php') ) {
+				include("loader.php");
+			}
+		?>
+
+		<!-- Old Browser Warning -->
+		<!--[if lt IE 9]>
+			<section class="container">
+				Did you know that your web browser is a bit old? Some of the content on this site might not work right as a result. <a href="http://whatbrowser.org">Upgrade your browser</a> for a faster, better, and safer web experience.
+			</section>
+		<![endif]-->
+		<!--[if lt IE 9]>
+			<section class="container">
+				Did you know that your web browser is a bit old? Some of the content on this site might not work right as a result. <a href="http://whatbrowser.org">Upgrade your browser</a> for a faster, better, and safer web experience.
+			</section>
+		<![endif]-->
+		<div class="jsdump" style="display:none;"></div>
+
+		<div id="page" class="hfeed site">
+			<div id="content" class="site-content">
